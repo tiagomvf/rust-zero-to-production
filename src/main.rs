6aa79de911f3fe205ce870e4e41fbedcbeedaff7
Tiago Maia <tiago.maia@trt6.jus.br>
2023-06-04 18:47:17 -0300

    Deploy to Digital Ocean

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 use zero2prod::configuration::get_configuration;
@@ -15,10 +15,11 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let db_poll = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2)).max_connections(100).min_connections(80)
+        .connect_lazy(configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address).expect("Failed do bind address");
-    run(listener, db_poll)?.await
+    run(listener, connection_pool)?.await
 }

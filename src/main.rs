18ea586ddd2a334f424919185b2a9d2b38e81e22
Tiago Maia <tiago.maia@trt6.jus.br>
2023-08-21 21:51:38 -0300

    Fix fmt and clippy errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::net::TcpListener;
 use secrecy::ExposeSecret;
 use sqlx::PgPool;
+use std::net::TcpListener;
 
 use zero2prod::configuration::get_configuration;
 
@@ -15,10 +15,10 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(
-        &configuration.database.connection_string().expose_secret()
-    ).await
-        .expect("Failed to connect to Postgres");
+    let connection_pool =
+        PgPool::connect(configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
